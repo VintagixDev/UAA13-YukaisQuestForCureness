8aeab179e102244a60f6fa94730f271b0bd30e3a
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-10-16 16:12:53 +0200

    Ajout de ressources 2D
    tileSheet d'environement

---
+++ Assets/Ressources/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//when something get into the alta, make the runes glow
+namespace Cainos.PixelArtTopDown_Basic
+{
+
+    public class PropsAltar : MonoBehaviour
+    {
+        public List<SpriteRenderer> runes;
+        public float lerpSpeed;
+
+        private Color curColor;
+        private Color targetColor;
+
+        private void Awake()
+        {
+            targetColor = runes[0].color;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            targetColor.a = 1.0f;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            targetColor.a = 0.0f;
+        }
+
+        private void Update()
+        {
+            curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+
+            foreach (var r in runes)
+            {
+                r.color = curColor;
+            }
+        }
+    }
+}
