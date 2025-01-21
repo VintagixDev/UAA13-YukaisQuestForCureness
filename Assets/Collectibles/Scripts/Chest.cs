15fed94f28a89d8f79a38413906e44f021d1d937
Claude Maël <230622@CCM-Asty-Moulin.local>
2025-01-21 11:51:29 +0100

    Chests

--- Assets/Collectibles/Scripts/Chest.cs
+++ Assets/Collectibles/Scripts/Chest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Chest : MonoBehaviour
@@ -10,12 +11,22 @@ public class Chest : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("collided");
         GameObject gameObject = collision.gameObject;
         if (gameObject.name == "Player" && isOpen == false)
         {
-            isOpen = true;
 
+            isOpen = true;
+            Vector3 pos = transform.position;
+            pos.y += 1;
+            pos.x -= 1;
+            foreach(GameObject item in Items)
+            {
+                pos.x += 1;
+                GameObject instanciated = Instantiate(item);
+                instanciated.transform.position = pos;
+                instanciated.name = item.name;
+                
+            }
 
         }
     }
